f3cb9b976ff5d956eead8243c38dc493c8f19796
Juan Luis <juanluivn@gmail.com>
2022-04-24 20:24:45 -0400

    Added Visualizer

---
+++ src/video_script_R3BP.m
@@ -0,0 +1,381 @@
+clc; close all; clear;
+
+% First-Order Linear
+%{
+omega = 2.0152105515;
+time_vec = 0:0.01:1/omega;
+scaling_factor = 1.495978714e8;
+w_p = 2.086453455;
+w_v = 2.0152105515;
+Ax = 206000/scaling_factor;
+Ay = 665000/scaling_factor;
+Az = 110000/scaling_factor;
+
+t = 0:0.05:2*pi;%100;
+angle1 = 0:0.01:2*pi;
+angle2 = 0:0.01:2*pi;
+
+xpoint = 0.932385;
+ypoint = 0;
+zpoint = 0;
+
+mu = -9.537e-4;
+xsun = -mu;
+ysun = 0;
+zsun = 0;
+xearth = 1 + xsun;
+yearth = 0;
+zearth = 0;
+
+theta = 0 : 0.01 : 2*pi;
+eradius = 6378/scaling_factor;
+sradius = 696000/scaling_factor;
+
+phi = 0;
+m = 1;
+psi = m*(pi/2) + phi;
+
+% PLOTS 1
+% x1 = -Ax*cos(angle1 + phi) + xpoint;
+% y1 = Ay*sin(angle1 + phi) + ypoint;
+% z1 = Az*sin(angle2 + 1*(pi/2) + phi) + zpoint;
+% x3 = -Ax*cos(angle1 + phi) + xpoint;
+% y3 = Ay*sin(angle1 + phi) + ypoint;
+% z3 = Az*sin(angle2 + 3*(pi/2) + phi) + zpoint;
+% 
+% subplot(3,1,1);
+% hold on;
+% plot(xpoint, ypoint, 'b*')
+% plot(xearth, yearth, 'bo', 'MarkerSize', 5)
+% plot(xsun, ysun, 'ro', 'MarkerSize', 10)
+% plot(x1, y1, 'r-', 'DisplayName', 'M = 1')
+% plot(x3, y3, 'k-', 'DisplayName', 'M = 3')
+% xlabel('X')
+% ylabel('Y')
+% legend()
+% hold off;
+% 
+% subplot(3,1,2);
+% hold on;
+% plot(xearth, zearth, 'bo', 'MarkerSize', 5)
+% plot(xsun, zsun, 'ro', 'MarkerSize', 10)
+% plot(xpoint, zpoint, 'b*')
+% plot(x1, z1, 'r-', 'DisplayName', 'M = 1')
+% plot(x3, z3, 'k-', 'DisplayName', 'M = 3')
+% xlabel('X') 
+% ylabel('Z') 
+% hold off;
+% 
+% subplot(3,1,3);
+% hold on;
+% plot(yearth, zearth, 'bo', 'MarkerSize', 5)
+% plot(ysun, zsun, 'ro', 'MarkerSize', 10)
+% plot(ypoint, zpoint, 'b*')
+% plot(y1, z1, 'r-', 'DisplayName', 'M = 1')
+% plot(y3, z3, 'k-', 'DisplayName', 'M = 3')
+% xlabel('Y') 
+% ylabel('Z')
+% hold off;
+
+% PLOTS 2
+% m = 1;
+% phi = 0;
+% psi = m*(pi/2) + phi;
+% x1 = -Ax*cos(w_p*t + phi) + xpoint;
+% y1 =  Ay*sin(w_p*t + phi) + ypoint;
+% z1 =  Az*sin(w_v*t + psi) + zpoint;
+% 
+% hold on;
+% [X,Y,Z] = sphere;
+% X2 = X * eradius;
+% Y2 = Y * eradius;
+% Z2 = Z * eradius;
+% XS = X * sradius;
+% YS = Y * sradius;
+% ZS = Z * sradius;
+% surf(X2 + xearth, Y2, Z2)
+% surf(XS + xsun, YS, ZS)
+% plot3(xpoint, ypoint, zpoint, 'b*')
+% plot3(x1, y1, z1, 'r', 'DisplayName', 'M = 1')
+% zlim([-0.01 0.01])
+% ylim([-0.01 0.01])
+% grid on;
+% xlabel('X') 
+% ylabel('Y') 
+% zlabel('Z') 
+% hold off;
+
+% PLOTS 3
+% m = 1;
+% phi = 0;
+% psi = m*(pi/2) + phi;
+% x1 = -Ax*cos(w_p*t + phi) + xpoint;
+% y1 =  Ay*sin(w_p*t + phi) + ypoint;
+% z1 =  Az*sin(w_v*t + psi) + zpoint;
+% 
+% hold on;
+% plot3(xpoint, ypoint, zpoint, 'b*')
+% plot3(xearth, yearth, zearth, 'bo', 'MarkerSize', 5)
+% plot3(xsun, ysun, zsun, 'ro', 'MarkerSize', 10)
+% plot3(x1, y1, z1, 'r', 'DisplayName', 'M = 1')
+% grid on;
+% xlabel('X') 
+% ylabel('Y') 
+% zlabel('Z') 
+% hold off;
+
+% PLOTS 4
+% m = 1;
+% phi = 0;
+% psi = m*(pi/2) + phi;
+% x1 = -Ax*cos(w_p*t + phi) + xpoint;
+% y1 =  Ay*sin(w_p*t + phi) + ypoint;
+% z1 =  Az*sin(w_v*t + psi) + zpoint;
+% subplot(3,1,1);
+% hold on;
+% plot(xpoint, ypoint, 'b*')
+% plot(xearth, yearth, 'bo', 'MarkerSize', 5)
+% plot(xsun, ysun, 'ro', 'MarkerSize', 10)
+% plot(x1, y1, 'r-', 'DisplayName', 'M = 1')
+% xlabel('X')
+% ylabel('Y')
+% legend()
+% hold off;
+% 
+% subplot(3,1,2);
+% hold on;
+% plot(xearth, zearth, 'bo', 'MarkerSize', 5)
+% plot(xsun, zsun, 'ro', 'MarkerSize', 10)
+% plot(xpoint, zpoint, 'b*')
+% plot(x1, z1, 'r-', 'DisplayName', 'M = 1')
+% xlabel('X') 
+% ylabel('Z') 
+% hold off;
+% 
+% subplot(3,1,3);
+% hold on;
+% plot(yearth, zearth, 'bo', 'MarkerSize', 5)
+% plot(ysun, zsun, 'ro', 'MarkerSize', 10)
+% plot(ypoint, zpoint, 'b*')
+% plot(y1, z1, 'r-', 'DisplayName', 'M = 1')
+% xlabel('Y') 
+% ylabel('Z')
+% hold off;
+
+%}
+
+
+
+
+% Third-Order Linear
+
+k = 3.2292680962;
+c2 = 4.0610735668;
+c3 = 3.0200105081;
+c4 = 3.0305378797;
+lam = sqrt((c2 + sqrt(9*c2^2 - 8*c2))/2);
+
+d1 = ((3*lam^2)/k)*(k*(6*lam^2 - 1) - 2*lam); 
+d2 = ((8*lam^2)/k)*(k*(11*lam^2 - 1) - 2*lam);
+a21 = 3*c3*(k^2 - 2)/(4*(1 + 2*c2)); 
+a22 = 3*c3/(4*(1 + 2*c2));
+
+a23 = -((3*c3*lam)/(4*k*d1))*(3*(k^3)*lam - 6*k*(k-lam) + 4); 
+a24 = -((3*c3*lam)/(4*k*d1))*(3*k*lam + 2);
+
+b21 = -3*c3*lam*(3*k*lam - 4)/(2*d1);
+b22 = -3*c3*lam/d1;
+
+a31 = (-9*lam/(4*d2))*(4*c3*(k*a23 - b21) + k*c4*(4 + k^2)) ...
+    + ((9*lam^2 + 1 - c2)/(2*d2))*(3*c3*(2*a23 - k*b21) + c4*(2 + 3*k^2));
+
+d21 = -c3/(2*lam^2);
+d31 = (3/(64*lam^2))*(4*c3*a24 + c4);
+d32 = (3/(64*lam^2))*(4*c3*(a23 - d21) + c4*(4 + k^2));
+
+a32 = (-9*lam/(4*d2))*(4*c3*(3*k*a24 - b22) + k*c4) ... 
+    - 3*((9*lam^2 + 1 - c2)/(2*d2))*(c3*(k*b22 - d21 - 2*a24) - c4);
+
+b31 = (3/(8*d2))*8*lam*(3*c3*(k*b21 - 2*a23) - c4*(2 + 3*k^2)) ... 
+     + (3/(8*d2))*((9*lam^2 + 1 + 2*c2)*(4*c3*(k*a23 - b21)) + k*c4*(4 + k^2));
+
+b32 = ((9*lam)/d2)*(c3*(k*b22 + d21 + -2*a24) - c4) ... 
+    + 3*((9*lam^2 + 1 + 2*c2)/(8*d2))*(4*c3*(k*a24 - b22) + k*c4); 
+
+xpoint = 0.932385;
+ypoint = 0;
+zpoint = 0;
+mu = -9.537e-4;
+xsun = -mu;
+ysun = 0;
+zsun = 0;
+xearth = 1 + xsun;
+yearth = 0;
+zearth = 0;
+
+phi = 0;
+w_p = 2.086453455;
+tau = 0:0.05:100;
+tau_1 = w_p*tau + phi;
+
+scaling_factor = 1.495978714e8;%1.497610041e6;
+eradius = 6378/scaling_factor;
+sradius = 696000/scaling_factor;
+Ax = 206000/scaling_factor;
+Ay = 665000/scaling_factor;
+Az = 110000/scaling_factor;
+m = 1;
+dm = 2 - m;
+x1 =    -Ax*cos(tau_1) + (a23*Ax^2 - a24*Az^2)*cos(2*tau_1) + (a31*Ax^3 - a32*Ax*Az^2)*cos(3*tau_1) + a21*Ax^2 + a22*Az^2 + xpoint;
+y1 =     Ay*sin(tau_1) + (b21*Ax^2 - b22*Az^2)*sin(2*tau_1) + (b31*Ax^3 - b32*Ax*Az^2)*sin(3*tau_1) + ypoint;
+z1 =  dm*Az*cos(tau_1) + dm*d21*Ax*Az*cos(2*tau_1 - 3) + dm*(d32*Az*Ax^2 - d31*Az^3)*cos(3*tau_1) + zpoint;
+
+hold on;
+plot3(xpoint, ypoint, zpoint, 'b*')
+[X,Y,Z] = sphere;
+X2 = X * eradius;
+Y2 = Y * eradius;
+Z2 = Z * eradius;
+XS = X * sradius;
+YS = Y * sradius;
+ZS = Z * sradius;
+surf(X2 + xearth, Y2, Z2)
+surf(XS + xsun, YS, ZS)
+plot3(x1, y1, z1, 'r', 'DisplayName', 'M = 1')
+grid on;
+xlabel('X') 
+ylabel('Y') 
+zlabel('Z') 
+hold off;
+
+%}
+
+
+
+
+% BACKUP
+% clc; close all; clear all;
+% 
+% omega = 2.0152105515;
+% 
+% time_vec = 0:0.01:1/omega;
+% 
+% % [x, y, z] = sphere(40);
+% 
+% scaling_factor = 1.497610041e6;
+% w_p = 2.086453455;
+% w_v = 2.0152105515;
+% A_x = 206000/scaling_factor;
+% A_z = 110000/scaling_factor;
+% K = 3.2292680962;
+% 
+% m = 1;
+% phi = 0;
+% psi = m*(pi/2) + phi;
+% 
+% angle1 = 0:0.01:2*pi;
+% angle2 = 0:0.01:2*pi;
+% 
+% xpoint = 0.932385;
+% ypoint = 0;
+% zpoint = 0;
+% 
+% mu = -9.537e-4;
+% xsun = -mu;
+% ysun = 0;
+% zsun = 0;
+% 
+% xearth = 1 + xsun;
+% yearth = 0;
+% zearth = 0;
+% 
+% % xr = -A_x*cos(angle1) + xpoint;
+% % yr = K*A_x*sin(angle1) + ypoint;
+% % zr = A_z*sin(angle2) + zpoint;
+% 
+% x1 = -A_x*cos(angle1 + phi) + xpoint;
+% y1 = K*A_x*sin(angle1 + phi) + ypoint;
+% z1 = A_z*sin(angle2 + 1*(pi/2) + phi) + zpoint;
+% 
+% x3 = -A_x*cos(angle1 + phi) + xpoint;
+% y3 = K*A_x*sin(angle1 + phi) + ypoint;
+% z3 = A_z*sin(angle2 + 3*(pi/2) + phi) + zpoint;
+% 
+% gamma = 1.001090475e-2;
+% % xbar_r = (xr - 1 + mu + gamma)/gamma;
+% % ybar_r = yr/gamma;
+% % zbar_r = zr/gamma;
+% 
+% xbar_1 = (x1 - 1 + mu + gamma)/gamma;
+% ybar_1 = y1/gamma;
+% zbar_1 = z1/gamma;
+% 
+% xbar_3 = (x3 - 1 + mu + gamma)/gamma;
+% ybar_3 = y3/gamma;
+% zbar_3 = z3/gamma;
+% 
+% % plot3(xr, yr, zr, 'b-*', 'DisplayName', 'Reference')
+% % hold on;
+% % plot3(x1, y1, z1, 'r-*', 'DisplayName', 'M = 1')
+% % plot3(x3, y3, z3, 'k-*', 'DisplayName', 'M = 3')
+% % hold off;
+% % legend()
+% % xlabel('X') 
+% % ylabel('Y') 
+% % zlabel('Z') 
+% 
+% 
+% subplot(3,1,1);
+% hold on;
+% plot(xpoint, ypoint, 'b*')
+% plot(xearth, yearth, 'bo', 'MarkerSize', 5)
+% plot(xsun, ysun, 'ro', 'MarkerSize', 10)
+% % plot(xr, yr, 'b-', 'DisplayName', 'Reference')
+% % plot(x1, y1, 'r-', 'DisplayName', 'M = 1')
+% % plot(x3, y3, 'k-', 'DisplayName', 'M = 3')
+% plot(xbar_1, ybar_1, 'r-', 'DisplayName', 'M = 1')
+% plot(xbar_3, ybar_3, 'k-', 'DisplayName', 'M = 3')
+% xlabel('X') 
+% ylabel('Y')
+% % xlim([0.4, 1.4])
+% legend()
+% hold off;
+% 
+% subplot(3,1,2);
+% hold on;
+% plot(xearth, zearth, 'bo', 'MarkerSize', 5)
+% plot(xsun, zsun, 'ro', 'MarkerSize', 10)
+% plot(xpoint, zpoint, 'b*')
+% % plot(xr, zr, 'b-')
+% % plot(x1, z1, 'r-')
+% % plot(x3, z3, 'k-')
+% plot(xbar_1, zbar_1, 'r-', 'DisplayName', 'M = 1')
+% plot(xbar_3, zbar_3, 'k-', 'DisplayName', 'M = 3')
+% xlabel('X') 
+% ylabel('Z') 
+% hold off;
+% 
+% subplot(3,1,3);
+% hold on;
+% plot(yearth, zearth, 'bo', 'MarkerSize', 5)
+% plot(ysun, zsun, 'ro', 'MarkerSize', 10)
+% plot(ypoint, zpoint, 'b*')
+% % plot(yr, zr, 'b-')
+% % plot(y1, z1, 'r-')
+% % plot(y3, z3, 'k-')
+% plot(ybar_1, zbar_1, 'r-', 'DisplayName', 'M = 1')
+% plot(ybar_3, zbar_3, 'k-', 'DisplayName', 'M = 3')
+% xlabel('Y') 
+% ylabel('Z') 
+% hold off;
+% 
+% % BACKUP
+% % x = -A_x*cos(time_vec.*w_p + phi);
+% % y = K*A_x*sin(time_vec.*w_p + phi);
+% % z = A_z*sin(time_vec.*w_v + psi);
+% 
+% % plot(y, z, 'b')
+% % hold on;
+% % plot(y_, z_, 'r')
+% % hold off;
+% 
